98b7242ba69a07d08b11c6583a84f977bcc7a5c8
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-07-29 17:48:00 +0200

    Moved Provider to index.js file

--- would-you-rather-final/src/App.js
+++ would-you-rather-final/src/App.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
-import {Provider} from 'react-redux'
-import { createStore } from "redux";
-import reducers from "./reducers"
 import './App.css';
 
 class App extends Component{
 
   render(){
     return (
-      <Provider store={createStore(reducers)}>
         <div>HI!</div>
-      </Provider>
     );
   }
 }
