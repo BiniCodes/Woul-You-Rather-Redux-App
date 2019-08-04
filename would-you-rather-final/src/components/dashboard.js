c80637dbbe6220f823eb9ffd921712c8873a7575
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-08-04 13:30:51 +0200

    Created Dashboard component

---
+++ would-you-rather-final/src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux'
+
+
+class Dashboard extends Component{
+    render() {
+        return(
+            <div>This is the Dashboard!</div>
+        )
+    }
+}
+
+export default Dashboard
