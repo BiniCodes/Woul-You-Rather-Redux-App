59d3efbf3e668420b4baf91097ce9a9bd37323ca
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-08-13 00:01:48 +0200

    Added Redux Dev Tools extension

--- would-you-rather-final/src/index.js
+++ would-you-rather-final/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
 import middleware from './middleware'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers, middleware);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    middleware
+  ));
 
 ReactDOM.render(
         <Provider store={store}>
