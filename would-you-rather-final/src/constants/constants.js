d7a84fb3d9b457a2d8473636ac83547bcac12093
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-08-15 11:12:07 +0200

    Added constants for adding a question and saving an answer

--- would-you-rather-final/src/constants/constants.js
+++ would-you-rather-final/src/constants/constants.js
@@ -1,5 +1,8 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
 export const RECEIVE_USERS = "RECEIVE-USERS";
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const ADD_QUESTION = "ADD_QUESTION";
+export const SAVE_ANSWER = "SAVE_ANSWER";
+
 
 
