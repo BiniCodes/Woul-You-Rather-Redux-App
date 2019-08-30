6a649edfc9cbf63f69ee0a26243cc221e4da715b
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-08-30 21:20:23 +0200

    Added sort algorithm

--- would-you-rather-final/src/components/Leaderboard.js
+++ would-you-rather-final/src/components/Leaderboard.js
@@ -6,48 +6,61 @@ import './style/index-leaderboard.css'
 
 class Leaderboard extends Component{
 
-    state= {
-        scores: [{id: "8xf0y6ziyjabvozdd253nd",
-            totalScore: 0}]
-    }
-
-
     render(){
-        const {users , userIds} = this.props
-        const {id} = this.state.scores
+        const {users} = this.props
+
+        const answeredQuestions = (x) =>{
+            return Object.keys(x.answers).length
+        }
+
+        const createdQuestions = (x) => {
+            return x.questions.length
+        }
+
+        const total = (x) => {
+            return (answeredQuestions(x) + createdQuestions(x))
+        }
+
+        const sort = (users) =>{
+            let leaderList = Object.values(users).sort((a, b) => {
+            let totalA = total(a)
+            let totalB = total(b)
+
+            return totalB - totalA
+        })
+        return leaderList;
+    }
 
         return(
             <div id="leaderboard">
+
                 <ul>
-                {userIds.map((userId) =>{
-                    const total = (userId)=>{
-                        return (Object.keys(users[userId].answers).length) + (users[userId].questions.length)
-                    }
-                return(
-                    <li id="leaderBoardItem" key={userId} >
-                        <img src={users[userId].avatarURL} alt={users[userId].name} />
-                        <div id="questionsSummary">
-                            <h2>{users[userId].name}</h2>  
-                            <p>Answered questions <span> {Object.keys(users[userId].answers).length}</span></p>  
-                            <p>Created questions <span>{users[userId].questions.length}</span></p>  
-                        </div>
-                        <div id="score">
-                            <h3>Score</h3>
-                            <p>{(Object.keys(users[userId].answers).length) + (users[userId].questions.length)}</p>
-                        </div>
-                    </li>
-                )}
-                )}
+
+                {sort(users).map((user) =>{
+                    return(
+                        <li id="leaderBoardItem" key={user.id} >
+                            <img src={user.avatarURL} alt={user.name} />
+                            <div id="questionsSummary">
+                                <h2>{user.name}</h2>  
+                                <p>Answered questions <span> {answeredQuestions(user)}</span></p>  
+                                <p>Created questions <span>{createdQuestions(user)}</span></p>  
+                            </div>
+                            <div id="score">
+                                <h3>Score</h3>
+                                <p>{answeredQuestions(user) + createdQuestions(user)}</p>
+                            </div>
+                        </li>
+                    )})
+                }
                 </ul>
             </div>
         )
     }
 }
 
-function mapStateToProps({ users } , {userIds}){
+function mapStateToProps({ users }){
     return{
         users,
-        userIds,
     }
 }
 
