ab007019b6176bc1680b5518888901c6e0fcdaf3
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-08-31 21:37:05 +0200

    Added Private Router with functionality

--- would-you-rather-final/src/components/PageContainer.js
+++ would-you-rather-final/src/components/PageContainer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component , Fragment} from 'react'
 import Navigation from './navigation.js'
 import Dashboard from './dashboard.js'
 import { connect } from 'react-redux'
@@ -6,37 +6,61 @@ import NewQuestion from './NewQuestion.js';
 import Login from './Login.js';
 import Results from './Results';
 import Leaderboard from './Leaderboard';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Switch, Route, Router, Redirect, Link } from "react-router-dom";
+import PollQuestion from './PollQuestion.js';
+
+
+const PrivateRoute = ({component: Component, authedUser, ...rest}) =>{
+    console.log(authedUser)
+    
+    return(
+        <Route {...rest} render={props => (
+                authedUser
+                ? <Component {...props} />
+                : <Redirect to="/login" />
+            )}
+        />
+    )           
+}
+
+// const PublicRoute = ({ component: Component, authedUser, restricted,...rest }) => {
+//     return (
+//         <Route {...rest} render={props => (
+//             authedUser && restricted
+//                 ? <Redirect to="/dashboard" />
+//                 : <Component {...props} />
+                 
+//         )}
+//         />
+//     )
+// }
 
 
 
 class PageContainer extends Component{
     render(){
+        let { authedUser } = this.props;
         return(
-            <Router>
-                <div>
-                    <Navigation/>
-                    <Route path="/login" component={Login} />
-                    <Route path="/dashboard/" exact component={() => <Dashboard ids={this.props.questionIds}/>} />
-                    <Route path="/newQuestion/" component={NewQuestion} />
-                    <Route path="/results/" component={() => <Results ids={this.props.questionIds} userIds={this.props.userIds}/>} />
-                    <Route path="/leaderboard/" component={() => <Leaderboard userIds={this.props.userIds}/>} />
-                    {/* <Dashboard ids={this.props.questionIds}/> */}
-                    {/* <NewQuestion/> */}
-                    {/* <Login/> */}
-                    {/* <Results ids={this.props.questionIds} userIds={this.props.userIds}/> */}
-                    {/* <Leaderboard userIds={this.props.userIds}/> */}
-                </div>
-            </Router>
+                <Switch>
+                        <Navigation/>
+                        <Route  path="/login" component={Login} authedUser={authedUser} />
+                        <PrivateRoute exact path="/dashboard/" component={Dashboard} ids={this.props.questionIds} authedUser={authedUser}/>
+                        <PrivateRoute path="/newQuestion/" component={NewQuestion} authedUser={authedUser} />
+                        {/* <Route path="/dashboard/answeredQuestions/results/" exact component={() => <Results ids={this.props.questionIds} userIds={this.props.userIds}/>} />
+                        <Route path="/dashboard/unAnsweredQuestions/questions/" exact component={() => <PollQuestion ids={this.props.questionIds} userIds={this.props.userIds} />} />                     */}
+                        <PrivateRoute path="/leaderboard/" component={Leaderboard} authedUser={authedUser} />
+
+                </Switch>
         )
     }
 }
 
-function mapStateToProps({ questions, users}) {
+function mapStateToProps({ questions, users , authedUser}) {
 
     return {
         questionIds: Object.keys(questions),
-        userIds: Object.keys(users)
+        userIds: Object.keys(users),
+        authedUser
     }
 }
 
