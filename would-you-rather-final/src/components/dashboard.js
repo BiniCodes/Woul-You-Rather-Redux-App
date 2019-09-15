425dfa1d7634c3e6e1994ddef842ab52a6c6891a
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-09-15 12:10:04 +0200

    mark buttons of answered and unanswered questions

--- would-you-rather-final/src/components/dashboard.js
+++ would-you-rather-final/src/components/dashboard.js
@@ -3,9 +3,6 @@ import { connect } from 'react-redux'
 import Question from './Question'
 import { BrowserRouter as Switch, Route, Link, NavLink } from "react-router-dom";
 import './style/index-dashboard.css'
-import PollQuestion from './PollQuestion';
-import Results from './Results';
-
 
 
 class Dashboard extends Component {
@@ -27,41 +24,49 @@ class Dashboard extends Component {
     }
 
     render() {
-
-        const { questions } = this.props
-        // https://reacttraining.com/react-router/web/example/sidebar
-        // https://stackoverflow.com/questions/33062830/using-react-router-with-a-layout-page-or-multiple-components-per-page
-
         return (
             <div id="listUserQuestions" className="columnView">
+
                 <div id="answeredUnanswered" className="rowView">
-                    <button id="unansweredQuestions" onClick={this.unanswerQuestion}>Unanswered</button>
-                    <button id="answeredQuestions" onClick={this.answerQuestion}>Answered</button>
+                    <button id="unansweredQuestions" 
+                            className={
+                                (this.state.answeredQuestion)
+                                ? " "
+                                : "isSelected"
+                            }
+                        onClick={this.unanswerQuestion}>Unanswered
+                    </button>
+
+                    <button id="answeredQuestions" 
+                        className={
+                            (this.state.answeredQuestion)
+                                ? "isSelected"
+                                : " "
+                        }
+                        onClick={this.answerQuestion}>Answered
+                    </button>
                 </div>
+
                 <ul>
                     {(this.state.answeredQuestion)
                         ? (
                             this.props.answeredIds.map((id) => (
                                 <li key={id}>
                                     {console.log(this.props)}
-                                    {/* {!((questions[questionId].optionOne.votes.includes(this.props.authedUser)) */}
-                                    {/* || (questions[questionId].optionTwo.votes.includes(this.props.authedUser))) */}
+
                                     <Question id={id} userIds={this.props.answeredIds} answered={this.state.answeredQuestion}/>
-                                    {/* // : null} */}
                                 </li>))
                         )
                         : (
                             this.props.unansweredIds.map((id) => (
                                 <li key={id}>
                                     {console.log(this.props)}
-                                    {/* {!((questions[questionId].optionOne.votes.includes(this.props.authedUser)) */}
-                                    {/* || (questions[questionId].optionTwo.votes.includes(this.props.authedUser))) */}
                                     <Question id={id} userIds={this.props.unansweredIds} answered={this.state.answeredQuestion}/>
-                                    {/* // : null} */}
                                 </li>))
                         )
                     }
                 </ul>
+
             </div>
         )
     }
@@ -80,6 +85,7 @@ function mapStateToProps({ questions, authedUser, users }) {
             .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
 
         authedUser,
+
         userIds: Object.keys(users),
     }
 }
