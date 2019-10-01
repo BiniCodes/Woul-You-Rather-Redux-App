32285d389a21b9cce731a8b1333a575be7c1b068
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-10-01 23:48:04 +0200

    Solved router errors

--- would-you-rather-final/src/components/PageContainer.js
+++ would-you-rather-final/src/components/PageContainer.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import Navigation from './navigation.js'
 import Dashboard from './dashboard.js'
 import { connect } from 'react-redux'
 import NewQuestion from './NewQuestion.js';
 import Login from './Login.js';
 import Leaderboard from './Leaderboard';
-import { BrowserRouter as Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import NotFound from './404.js'
 import QuestionId from './QuestionId.js';
 
@@ -31,15 +31,19 @@ class PageContainer extends Component{
     render(){
         let { authedUser } = this.props;
         return(
-                <Switch>
+                <Router>
+                    <Fragment>
                         <Navigation/>
-                        <Route exact path="/" component={Login} authedUser={authedUser} />
-                        <PrivateRoute exact path="/dashboard" component={Dashboard} ids={this.props.questionIds} authedUser={authedUser}/>
-                        <PrivateRoute exact path="/questions/:id" component={QuestionId} ids={this.props.questionIds} userIds={this.props.userIds} authedUser={authedUser} />
-                        <PrivateRoute exact path="/add/" component={NewQuestion} authedUser={authedUser} />
-                        <PrivateRoute exact path="/leaderboard/" component={Leaderboard} authedUser={authedUser} />
-                        <PrivateRoute path="/error" component={NotFound}/>
-                </Switch>
+                        <Switch>
+                            <Route exact path="/" component={Login} authedUser={authedUser} />
+                            <PrivateRoute exact path="/dashboard" component={Dashboard} ids={this.props.questionIds} authedUser={authedUser}/>
+                            <PrivateRoute exact path="/questions/:id" component={QuestionId} ids={this.props.questionIds} userIds={this.props.userIds} authedUser={authedUser} />
+                            <PrivateRoute exact path="/add/" component={NewQuestion} authedUser={authedUser} />
+                            <PrivateRoute exact path="/leaderboard/" component={Leaderboard} authedUser={authedUser} />
+                            <Route exact component={NotFound}/>
+                        </Switch>
+                    </Fragment>
+                </Router>
         )
     }
 }
