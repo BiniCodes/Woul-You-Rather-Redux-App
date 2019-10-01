bb37b1d991a6e55a5a621b2b8c4e734378c0fb18
Sabrina Jodexnis <sabrinajodexnis@googlemail.com>
2019-10-01 10:55:17 +0200

    Added validation for undefined question

--- would-you-rather-final/src/utils/helpers.js
+++ would-you-rather-final/src/utils/helpers.js
@@ -2,15 +2,20 @@ export function formatQuestion(question, author, authedUser){
     const { id, timestamp, optionOne, optionTwo } = question
     const {name , avatarURL} = author
 
-    return{
-        name,
-        avatar: avatarURL,
-        questionId: id,
-        timestamp,
-        textOne: optionOne.text,
-        textTwo: optionTwo.text,
-        choseOptionOne: optionOne.votes.includes(authedUser),
-        choseOptionTwo: optionTwo.votes.includes(authedUser),
+    if(question === undefined){
+        alert("This question does not exist.")
+    } else {
+        return {
+            name,
+            avatar: avatarURL,
+            questionId: id,
+            timestamp,
+            textOne: optionOne.text,
+            textTwo: optionTwo.text,
+            choseOptionOne: optionOne.votes.includes(authedUser),
+            choseOptionTwo: optionTwo.votes.includes(authedUser),
+
+        }
 
     }
 }
